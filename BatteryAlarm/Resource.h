18091085e7cca78ddafe9e68c55bea597137e01b
mitko-z <winsett@abv.bg>
2016-11-19 09:25:19 +0200

    Icon changed to custom one

--- BatteryAlarm/Resource.h
+++ BatteryAlarm/Resource.h
@@ -1,5 +1,5 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Developer Studio generated include file.
+// Microsoft Visual C++ generated include file.
 // Used by BatteryAlarm.rc
 //
 #define IDM_ABOUTBOX                    0x0010
@@ -16,7 +16,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_RESOURCE_VALUE        131
 #define _APS_NEXT_COMMAND_VALUE         32773
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
